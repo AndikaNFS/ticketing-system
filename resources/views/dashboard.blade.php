afb2a4a185a73d9bf8cbaae7313effdac4cb57bd
andikanfs <andikanursasmito5@gmail.com>
2025-04-04 14:39:31 +0700

    remove header & change logo

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,11 @@
 
 
 <x-app-layout>
-    <x-slot name="header">
+    {{-- <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Ticketing') }}
         </h2>
-    </x-slot>
+    </x-slot> --}}
     
 
     <div class="py-12">
