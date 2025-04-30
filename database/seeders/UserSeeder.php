18bdf8e9636a9dcc1d73bf1da3dd66bb77819d62
andikanfs <andikanursasmito5@gmail.com>
2025-04-30 13:06:33 +0700

    user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,68 +16,66 @@ class UserSeeder extends Seeder
     public function run(): void
     {
 
-        $superAdminRole = Role::create([
-            'name' => 'superadmin'
-        ]);
+        // $superAdminRole = Role::create([
+        //     'name' => 'superadmin'
+        // ]);
 
-        $adminRole = Role::create([
-            'name' => 'admin'
-        ]);
+        // $adminRole = Role::create([
+        //     'name' => 'admin'
+        // ]);
 
-        $userRole = Role::create([
-            'name' => 'user'
-        ]);
+        // $userRole = Role::create([
+        //     'name' => 'user'
+        // ]);
 
-        $userSuperAdmin = User::create([
-            'name' => 'SuperAdmin',
-            'email' => 'superadmin@gmail.com',
-            'password' => bcrypt('*mhb2020')
-        ]);
-        $userAdmin = User::create([
-            'name' => 'Admin',
-            'email' => 'admin@gmail.com',
-            'password' => bcrypt('*rci2120')
-        ]);
+        // $userSuperAdmin = User::create([
+        //     'name' => 'SuperAdmin',
+        //     'email' => 'superadmin@gmail.com',
+        //     'password' => bcrypt('*mhb2020')
+        // ]);
+        // $userAdmin = User::create([
+        //     'name' => 'Admin',
+        //     'email' => 'admin@gmail.com',
+        //     'password' => bcrypt('*rci2120')
+        // ]);
 
-        $user = User::create([
+        // $user = User::create([
+        //         'name' => 'User',
+        //         'email' => 'user@gmail.com',
+        //         'password' => bcrypt('userlogin')
+        // ]);
+
+        // $userSuperAdmin->assignRole($superAdminRole);
+        // $userAdmin->assignRole($adminRole);
+        // $user->assignRole($userRole);
+
+        DB::table('users')->insert([
             [
-                'name' => 'User',
-                'email' => 'user@gmail.com',
-                'password' => bcrypt('userlogin')
+            'name' => 'Admin IT',
+            'email' => 'itrrchocolate@gmail.com',
+            'password' => bcrypt('*mhb2020'),
+
+            ],
+            [
+            'name' => 'Andika Nur Sasmito',
+            'email' => 'andika@gmail.com',
+            'password' => bcrypt('andikans'),
+            ],
+            [
+            'name' => 'Santo',
+            'email' => 'santo@gmail.com',
+            'password' => bcrypt('santo123'),
+            ],
+            [
+            'name' => 'Asep',
+            'email' => 'asep@gmail.com',
+            'password' => bcrypt('asep123'),
+            ],
+            [
+            'name' => 'Kodam',
+            'email' => 'kodam@gmail.com',
+            'password' => bcrypt('kodam123'),
             ],
         ]);
-
-        $userSuperAdmin->assignRole($superAdminRole);
-        $userAdmin->assignRole($adminRole);
-        $user->assignRole($userRole);
-
-        // DB::table('users')->insert([
-        //     [
-        //     'name' => 'Admin IT',
-        //     'email' => 'itrrchocolate@gmail.com',
-        //     'password' => bcrypt('*mhb2020'),
-
-        //     ],
-        //     [
-        //     'name' => 'Andika Nur Sasmito',
-        //     'email' => 'andika@gmail.com',
-        //     'password' => bcrypt('andikans'),
-        //     ],
-        //     [
-        //     'name' => 'Santo',
-        //     'email' => 'santo@gmail.com',
-        //     'password' => bcrypt('santo123'),
-        //     ],
-        //     [
-        //     'name' => 'Asep',
-        //     'email' => 'asep@gmail.com',
-        //     'password' => bcrypt('asep123'),
-        //     ],
-        //     [
-        //     'name' => 'Kodam',
-        //     'email' => 'kodam@gmail.com',
-        //     'password' => bcrypt('kodam123'),
-        //     ],
-        // ]);
     }
 }
