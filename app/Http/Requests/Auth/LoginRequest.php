83a3258d7627863053faf003d5322074776b04f5
andikanfs <andikanursasmito5@gmail.com>
2025-06-01 11:24:22 +0700

    update remember me

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -29,7 +29,7 @@ class LoginRequest extends FormRequest
         return [
             'email' => ['required', 'string', 'email'],
             'password' => ['required', 'string'],
-            'remember' => ['nullable|in:on,off'],
+            'remember' => ['nullable', 'in:on,off'],
         ];
     }
 
