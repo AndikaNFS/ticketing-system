051c68dc49dad0db67089ed730a28788a47d3c44
andikanfs <andikanursasmito5@gmail.com>
2025-06-28 21:53:12 +0800

    change selected vendor edit

--- resources/views/building/tickets/edit.blade.php
+++ resources/views/building/tickets/edit.blade.php
@@ -45,7 +45,7 @@
             <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Vendor</label>
             {{-- <input value="{{ $building->vendors->name }}" type="text" name="vendor_id" id="vendor_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=" " required /> --}}
             <select id="vendor_id" name="vendor_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                <option selected >Pilih Vendor</option>
+                <option selected value="">Pilih Vendor</option>
                 @foreach ($vendors as $vendor)
                     <option value="{{ $vendor->id}}"  {{ $building->vendor_id == $vendor->id ? 'selected' : ''}}>{{ $vendor->name }}</option>
                 @endforeach
