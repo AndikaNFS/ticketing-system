bb7913817a416e1a3de95519dfc85a9c6b13d346
andikanfs <andikanursasmito5@gmail.com>
2025-06-28 16:01:56 +0800

    fix bug

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -34,7 +34,7 @@
                     @if(auth()->user()->hasRole('superadmin|admin|building|maintenance|maintenance1'))
 
                     
-                    <button id="dropdownDividerButton" data-dropdown-toggle="dropdownDivider" class="text-white  font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center " type="button">Building <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    <button id="dropdownDividerButton" data-dropdown-toggle="dropdownDivider" class="text-gray-500  font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center " type="button">Building <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                     </svg>
                     </button>
