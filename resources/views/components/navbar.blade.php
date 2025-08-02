060db645072eb375ddfade6e9df6a080e1a4518e
andikanfs <andikanursasmito5@gmail.com>
2025-08-02 12:32:39 +0700

    fix link logo

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -8,12 +8,12 @@
                <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
             </svg>
          </button>
-        <a href="https://flowbite.com" class="flex ms-2 md:me-24">
+        {{-- <a href="https://flowbite.com" class="flex ms-2 md:me-24"> --}}
           <img src="/images/logo-rr.png" alt="RR Chocolate" width="40px" height="40px"  srcset="" class="mt-2">
 
           {{-- <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 me-3" alt="FlowBite Logo" /> --}}
           <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">The RR Chocolate</span>
-        </a>
+        {{-- </a> --}}
       </div>
       <div class="flex items-center">
           <div class="flex items-center ms-3">
