07e65b5c8ef31d3bab5e3d53f6b63b5c3ec1fb8b
andikanfs <andikanursasmito5@gmail.com>
2025-09-23 21:54:11 +0700

    edit modal

--- resources/views/visits/index.blade.php
+++ resources/views/visits/index.blade.php
@@ -39,7 +39,7 @@
                 <div class=" flex place-content-end">
                     <!-- Modal toggle -->
                     <button data-modal-target="default-modal" data-modal-toggle="default-modal" class="block text-white w-full sm:w-auto bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-                    Filter Ticketing
+                    Filter Visit
                     </button>
 
                     <!-- Main modal -->
