e4de061bee73dcc82c2c63486523a9f0176783d9
andikanfs <andikanursasmito5@gmail.com>
2026-02-28 17:35:21 +0700

    fix typo

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -165,7 +165,7 @@
                </svg>
 
 
-               <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Master Data</span>
+               <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Data Master</span>
                <svg class="w-3 h-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                </svg>
