9ac5a227dfe01656da97a1b1ef42593d09a2263e
AndikaNFS <andikanursasmito5@gmail.com>
2026-06-29 20:10:13 +0700

    fix count

--- resources/views/outlets/count.blade.php
+++ resources/views/outlets/count.blade.php
@@ -42,9 +42,9 @@
                                 </table>
                         </div>
 
-                        <div class="flex items-center ">
+                        <!-- <div class="flex items-center ">
                             <button data-modal-hide="add-modal" type="submit" class="text-white bg-blue-700 w-full hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-                        </div>
+                        </div> -->
                     </div>
 
                 </div>
