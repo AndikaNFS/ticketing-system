674985fe4e6376567c4caf69bc38e77868567706
AndikaNFS <andikanursasmito5@gmail.com>
2026-07-06 12:15:40 +0700

    fix layout button count

--- resources/views/outlets/index.blade.php
+++ resources/views/outlets/index.blade.php
@@ -3,17 +3,19 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             
             <h1 class="text-gray-800 dark:text-gray-100 text-3xl m-5 max-w-md mx-auto text-center">Area Outlet</h1>
-            <div class="flex place-content-end">
+            <div class="flex place-content-end pr-4">
                     @if (auth()->user()->hasRole('admin|superadmin'))
                     
-                        
-                    <button data-modal-target="add-modal" data-modal-toggle="add-modal" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 mr-4 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Count</button>   
-                    <a href="{{ route('outlets.create') }}">
-                       <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add Area</button>   
-                    </a>
+                    <div class="flex flex-nowrap gap-2">
+                        <button data-modal-target="add-modal" data-modal-toggle="add-modal" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 mr-4 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Count</button>   
+                        <a href="{{ route('outlets.create') }}">
+                           <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add Area</button>   
+                        </a>
+
+                    </div> 
                     
                     @endif
-                    <div id="add-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                    <div id="count-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
                         @include('outlets.count')
                     </div>
     
