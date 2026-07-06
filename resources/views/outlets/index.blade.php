ea27858be33b73177d515c28db720a69c98b26ec
AndikaNFS <andikanursasmito5@gmail.com>
2026-07-06 12:19:44 +0700

    fix add modal count

--- resources/views/outlets/index.blade.php
+++ resources/views/outlets/index.blade.php
@@ -15,7 +15,7 @@
                     </div> 
                     
                     @endif
-                    <div id="count-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                    <div id="add-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
                         @include('outlets.count')
                     </div>
     
